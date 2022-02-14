ddc86798828386a931bb005a25821ed94f561289
Andrii Kulta <andriikulta@microsoft.com>
2022-02-14 14:19:22 +0100

    #test exceptions example

---
+++ dotnet-testing/ExceptionUnitTesting/ExceptionUnitTesting.Tests/ValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ExceptionUnitTesting.Tests
+{
+    public class ValidatorTests
+    {
+        private readonly Validator _validator = new Validator();
+
+        [Test]
+        public void Validate_ShouldNotThrow_WhenModeIsValid()
+        {
+            // Arrange
+            var model = new InputModel
+            {
+                Name = "name"
+            };
+
+            // Act 
+            Action validate = () => _validator.Validate(model);
+
+            // Assert
+            validate.Should().NotThrow();
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void Validate_ShouldThrow_WhenInvalidName(string name)
+        {
+            // Arrange
+            var model = new InputModel
+            {
+                Name = name
+            };
+
+            // Act 
+            Action validate = () => _validator.Validate(model);
+
+            // Assert
+            validate.Should().Throw<ValidationException>("invalid name");
+        }
+
+        [Test]
+        public void ValidateAsync_ShouldNotThrow_WhenModeIsValid()
+        {
+            // Arrange
+            var model = new InputModel
+            {
+                Name = "name"
+            };
+
+            // Act 
+            Func<Task> validate = () => _validator.ValidateAsync(model);
+
+            // Assert
+            validate.Should().NotThrowAsync();
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void ValidateAsync_ShouldThrow_WhenInvalidName(string name)
+        {
+            // Arrange
+            var model = new InputModel
+            {
+                Name = name
+            };
+
+            // Act 
+            Func<Task> validate = () => _validator.ValidateAsync(model);
+
+            // Assert
+            validate.Should().ThrowAsync<ValidationException>("invalid name");
+        }
+    }
+}
---
+++ dotnet-testing/ExceptionUnitTesting/ExceptionUnitTesting/Validator.cs
@@ -0,0 +1,40 @@
+﻿namespace ExceptionUnitTesting
+{
+    public class InputModel
+    {
+        public string Name { get; set; }
+    }
+
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+        {
+        }
+
+        public ValidationException(string message) : base(message) { }
+
+        public ValidationException(string message, Exception ex) : base(message, ex) { }
+
+    }
+
+    public class Validator
+    {
+        public void Validate(InputModel model)
+        {
+            if (string.IsNullOrEmpty(model.Name))
+            {
+                throw new ValidationException("invalid name");
+            }
+        }
+
+        public Task ValidateAsync(InputModel model)
+        {
+            if (string.IsNullOrEmpty(model.Name))
+            {
+                throw new ValidationException("invalid name");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
